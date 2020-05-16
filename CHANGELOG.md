eccb8d0de158b938952a1887fe1c5d32507e231b
ziven27 <zhengchengzhong@yuewen.com>
2020-05-16 11:58:29 +0800

    v1.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,27 +1,21 @@
-# [1.1.0](https://github.com/nu-system/nu-cli/compare/v1.0.6...v1.1.0) (2020-04-25)
+## 1.1.2 (2020-05-16)
 
-### Features
-
-- add husky ([78270b8](https://github.com/nu-system/nu-cli/commit/78270b8ea7660f3a0556bb0a3af1272fc69488db))
-- add test config ([8651156](https://github.com/nu-system/nu-cli/commit/86511561f0366e73a1a9549a5cf6ebd87eb6d0be))
-- update react ([fed2b19](https://github.com/nu-system/nu-cli/commit/fed2b194d789ad5bd9991b536dbe944a6ae9f689))
-- 添加 commit lint ([e186dd6](https://github.com/nu-system/nu-cli/commit/e186dd6dcaac0e41511ca5d0085cb974edaee6ca))
-
-## [1.0.6](https://github.com/nu-system/nu-cli/compare/v1.0.5...v1.0.6) (2020-04-15)
-
-## [1.0.5](https://github.com/nu-system/nu-cli/compare/v1.0.4...v1.0.5) (2020-04-15)
-
-### Features
-
-- add react ([5ef4f34](https://github.com/nu-system/nu-cli/commit/5ef4f3434d268f512b3a06fbedb08c9e6e5fde50))
-
-## [1.0.4](https://github.com/nu-system/nu-cli/compare/93e54a5d736cc87fef229188c1cd303c67ca56e4...v1.0.4) (2020-04-15)
 
 ### Bug Fixes
 
-- fix file ([9859946](https://github.com/nu-system/nu-cli/commit/98599468da6fca0b95b9ca3ca6d780d7febbfdcd))
+* fix file ([9859946](https://github.com/nu-system/nu-cli/commit/98599468da6fca0b95b9ca3ca6d780d7febbfdcd))
+
 
 ### Features
 
-- update ([93e54a5](https://github.com/nu-system/nu-cli/commit/93e54a5d736cc87fef229188c1cd303c67ca56e4))
-- 更新 files ([0effa18](https://github.com/nu-system/nu-cli/commit/0effa18065333d869500f7a50c69c5fda1293673))
+* add husky ([78270b8](https://github.com/nu-system/nu-cli/commit/78270b8ea7660f3a0556bb0a3af1272fc69488db))
+* add react ([5ef4f34](https://github.com/nu-system/nu-cli/commit/5ef4f3434d268f512b3a06fbedb08c9e6e5fde50))
+* add test config ([8651156](https://github.com/nu-system/nu-cli/commit/86511561f0366e73a1a9549a5cf6ebd87eb6d0be))
+* update ([93e54a5](https://github.com/nu-system/nu-cli/commit/93e54a5d736cc87fef229188c1cd303c67ca56e4))
+* update react ([fed2b19](https://github.com/nu-system/nu-cli/commit/fed2b194d789ad5bd9991b536dbe944a6ae9f689))
+* 发布新版本 ([cfc8448](https://github.com/nu-system/nu-cli/commit/cfc84484b6186f81d9610e56c83562041fe108bf))
+* 更新files ([0effa18](https://github.com/nu-system/nu-cli/commit/0effa18065333d869500f7a50c69c5fda1293673))
+* 添加 commit lint ([e186dd6](https://github.com/nu-system/nu-cli/commit/e186dd6dcaac0e41511ca5d0085cb974edaee6ca))
+
+
+
--- src/Creator.js
+++ src/Creator.js
@@ -1,15 +1,15 @@
-const chalk = require('chalk');
-const inquirer = require('inquirer');
+const chalk = require("chalk");
+const inquirer = require("inquirer");
 
 function Creator() {
   // 存储命令行获取的数据，作为demo这里只要这两个；
   this.options = {
     // 作者名称
-    author: '',
+    author: "",
     // 组件类型
-    componentType: '',
+    componentType: "",
     // 组件名称
-    componentName: '',
+    componentName: ""
   };
   this.init();
 }
@@ -21,19 +21,21 @@ Creator.prototype.init = function() {
   this.ask().then(answers => {
     this.options = Object.assign({}, this.options, answers);
     // 包名，也是文件夹的名字
-    const dirName = `${this.options.componentType}-${this.options.componentName}`;
-    const builder = require('./builder.js');
+    const dirName = `${
+      this.options.componentType
+    }-${this.options.componentName.toLowerCase()}`;
+    const builder = require("./builder.js");
     console.log(`\n👉 [${dirName}] is building...`);
     builder(this.options, dirName)
       .then(() => {
         console.log(chalk.green(`\n👇 [${dirName}] build success`));
         console.log(
           `install: ${chalk.yellow(`cd ${dirName}`)} && ${chalk.yellow(
-            'npm install',
-          )}`,
+            "npm install"
+          )}`
         );
-        console.log(`start: ${chalk.yellow('npm start')}`);
-        console.log(`build: ${chalk.yellow('npm build')} \n`);
+        console.log(`start: ${chalk.yellow("npm start")}`);
+        console.log(`build: ${chalk.yellow("npm build")} \n`);
       })
       .catch(err => {
         console.log(chalk.red(`\n❌ [${dirName}] build error`));
@@ -47,46 +49,46 @@ Creator.prototype.ask = function() {
   // 返回promise
   return inquirer.prompt([
     {
-      type: 'list',
-      name: 'componentType',
-      message: 'Select nu component type you wanna create:',
+      type: "list",
+      name: "componentType",
+      message: "Select nu component type you wanna create:",
       choices: [
-        { name: 'CSS', value: 'css' },
-        { name: 'React', value: 'react' },
-        { name: 'HTML', value: 'html' },
-        { name: 'Javascript', value: 'js' },
-        { name: 'VUE', value: 'vue' },
-        { name: 'React Native', value: 'rn', disabled: true },
-      ],
+        { name: "CSS", value: "css" },
+        { name: "React", value: "react" },
+        { name: "HTML", value: "html" },
+        { name: "Javascript", value: "js" },
+        { name: "VUE", value: "vue" },
+        { name: "React Native", value: "rn", disabled: true }
+      ]
     },
     {
-      name: 'componentName',
-      message: 'Input component name:',
+      name: "componentName",
+      message: "Input component name:",
       validate(input) {
         if (!input) {
-          return 'Input component name';
+          return "Input component name";
         }
         const p = new RegExp(/^[a-zA-Z][a-zA-Z0-9]+/);
         if (!p.test(input)) {
-          return 'Component name only surport number and english words';
+          return "Component name only surport number and english words";
         }
         return true;
-      },
+      }
     },
     {
-      name: 'author',
-      message: 'Input author name:',
+      name: "author",
+      message: "Input author name:",
       validate(input) {
         if (!input) {
-          return 'Input author name';
+          return "Input author name";
         }
         const p = new RegExp(/^[a-zA-Z][a-zA-Z0-9]+/);
         if (!p.test(input)) {
-          return 'Author name only surport number and english words';
+          return "Author name only surport number and english words";
         }
         return true;
-      },
-    },
+      }
+    }
   ]);
 };
 
